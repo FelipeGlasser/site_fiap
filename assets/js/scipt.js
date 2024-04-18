b92cdc090061111c23e92f45b5742154b20f3883
FelipeGlasser <106420318+FelipeGlasser@users.noreply.github.com>
2024-04-18 12:46:52 -0300

    Add files via upload

---
+++ assets/js/scipt.js
@@ -0,0 +1,28 @@
+console.log("calcular...")
+
+//manipulação do DOM 
+
+//let = local
+//var = global
+//const = constante
+
+document.querySelector("#qtde").addEventListener("change", calcular)
+document.querySelector("#servico").addEventListener("change", calcular)
+document.querySelector("#engenheiro_sim").addEventListener("change", calcular)
+document.querySelector("#engenheiro_nao").addEventListener("change", calcular)
+
+function calcular(){
+    const qtde = document.querySelector("#qtde").value
+    let preco = qtde * 1000
+
+    const tipo = document.querySelector("#servico").value
+    if (tipo == 2) preco += 1000
+
+    const temEngenheiro = document.querySelector("#engenheiro_sim").checked
+    if (temEngenheiro) preco *= 1.1
+
+    console.log (preco)
+
+    document.querySelector("#servico").innertext = "R$" + preco.toFixed(2)
+
+}
